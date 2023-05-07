8704fe2ead38a3a6ec05a3ac2204373d78051833
Anna <michalowska.aneczka@gmail.com>
2023-05-07 20:57:26 +0200

    last_commit

--- app.js
+++ app.js
@@ -27,8 +27,4 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
 module.exports = app;
--- tests/users.test.js
+++ tests/users.test.js
@@ -1,19 +1,33 @@
-const express = require("express");
 const request = require("supertest");
-const users = require("../models/users");
-const { app } = require("../app");
+const mongoose = require("mongoose");
+const app = require("./../app");
 
-app.post("/api/users/signup", users.signup);
+require("dotenv").config();
 
-describe("registration controller testing", () => {
-  beforeAll(() => {
-    app.listen(5555);
-    console.log("Wykonać na początku testów");
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.DB_HOST;
+
+const connection = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connection
+  .then(() => {
+    app.listen(PORT, function () {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((e) => {
+    console.log("Database connection error:", e);
+    process.exit(1);
   });
 
+describe("registration controller testing", () => {
   test("registration controller testing", async () => {
-    const response = await request(app).post("/api/users/signup");
+    const response = await request(app)
+      .post("/api/users/login")
+      .send({ email: "test@test.pl", password: "test123" });
     expect(response.status).toBe(200);
-    console.log(response.status);
-  }, 100000);
+  }, 50000);
 });
